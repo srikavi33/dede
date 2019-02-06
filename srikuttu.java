73c1a2d0de1aa031acb51ddd1ca8cda06f1dc65b
srikavi33 <47377676+srikavi33@users.noreply.github.com>
2019-02-06 15:30:16 +0530

    Create srikuttu.java

---
+++ srikuttu.java
@@ -0,0 +1,14 @@
+ class main
+{
+    public static void main(String[] args){
+        char ch ='O';
+        if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+        {
+        System.out.println("vowel");
+        }
+        else
+        {
+        System.out.println(" consonant");
+        }
+    }
+}
